2b39fd817c206f8dc1ffab513ae29f001562880c
satyam126 <satyam126@users.noreply.github.com>
2020-03-05 15:05:21 +0100

    Update master.sh

--- master/master.sh
+++ master/master.sh
@@ -15,7 +15,7 @@ kubectl create secret -n kube-system generic weave-passwd --from-file=/var/lib/w
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&password-secret=weave-passwd&env.IPALLOC_RANGE=192.168.0.0/24"
 
 # nginx-ingress-service
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/ASRRtechnologies/kubernetes-config/master/master/yaml/nginx-ingress-controller
 kubectl apply -f https://raw.githubusercontent.com/ASRRtechnologies/kubernetes-config/master/master/yaml/nginx-ingress-service.yaml
 
 # cert-manager
